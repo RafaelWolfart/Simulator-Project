6a736b6254956417e6be34ea05d52bf44b73ba07
Rafael Wolfart <rafaelwolfart1@outlook.com>
2025-06-21 13:28:31 -0300

    corrección de error al eliminar elementos del carrito

--- Js/carrito.js
+++ Js/carrito.js
@@ -1,16 +1,32 @@
 let carritoCompra = document.getElementById("carrito-content");
 
-let carritoCargado = localStorage.getItem("carrito");
+let carritoCargado = localStorage.getItem("carrito") || "[]";
 carritoCargado = JSON.parse(carritoCargado);
 
 function mostrarCarrito(itemCarrito) {
-    itemCarrito.forEach((producto) => {
-        const cardCompra = document.createElement("div");
-        cardCompra.innerHTML = `<h3>${producto.nombre}</h3>
+  itemCarrito.forEach((producto) => {
+    const cardCompra = document.createElement("div");
+    cardCompra.innerHTML = `<h3>${producto.nombre}</h3>
                                 <p>$${producto.precio}</p>
-                                <button class="eliminar-item">Eliminar</button>`;
-        carritoCompra.appendChild(cardCompra);
-    });
+                                <button class="eliminar-item" id="${producto.id}">Eliminar</button>`;
+    carritoCompra.appendChild(cardCompra);
+  });
 }
 
 mostrarCarrito(carritoCargado);
+
+const eliminarItem = document.querySelectorAll(".eliminar-item");
+eliminarItem.forEach(
+  (boton) =>
+    (boton.onclick = (e) => {
+      const itemID = e.currentTarget.id;
+      const itemIndex = carritoCargado.findIndex((item) => item.id == itemID);
+      if (itemIndex !== -1) {
+        carritoCargado.splice(itemIndex, 1);
+        console.log("producto eliminado");
+      }
+      localStorage.setItem("carrito", JSON.stringify(carritoCargado));
+    })
+);
+
+console.log(carritoCargado);
